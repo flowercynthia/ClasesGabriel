fa4cb7f8e312a123ab39e7eb8b6612cc7d603487
unknown <cynthia.flores@nestrixcorp.com>
2016-09-21 13:57:36 -0600

    first commit Clases metodos

--- Clases_con_Gabriel/Form1.cs
+++ Clases_con_Gabriel/Form1.cs
@@ -12,29 +12,7 @@ using System.Windows.Forms;
 
 namespace Clases_con_Gabriel
 {
-    public class ItemCombo
-    {
-        public ItemCombo() 
-        { 
-        
-        }
-        
-        public ItemCombo(string v, string t)
-        {
-            this.value = v;
-            this.text = t;
-        }
-
-        public string value
-        { 
-            get; set;
-        }
-
-        public string text
-        {
-            get; set;
-        }
-    }
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -56,14 +34,35 @@ namespace Clases_con_Gabriel
             //var items = new ItemCombo[3];
 
             //items[0] = new ItemCombo("2", "Movistar");
+            int h = 5;
+            int g = 4;
+            int m;
 
+           // multiplicar_opcional(g,h);
+
+            var valor = multiplicar_valor(h);
+            var refer = multiplicar_ref(ref g);
+            var refout = multiplicar_refout(out m, 8);
+
+            MessageBox.Show(valor + ": " + h);
+            MessageBox.Show(refer + ": " + g);
+            MessageBox.Show(refout + ": " + m);
+
+            eliminar(8, "2", "4", "89", "2");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             PagoFactura fact = new PagoFactura();
+            
 
             Factura data = new Factura();
+            Factura data2 = data;
+
+            data.Colector = "primero";
+            data2.Colector = "lo que sea";
+
+            MessageBox.Show(data.Colector);
 
             data.Colector = (cmbColectores.SelectedItem as ItemCombo).text;
             data.FechaCorte = dtFechaCorte.Value;
@@ -76,7 +75,74 @@ namespace Clases_con_Gabriel
             var result = fact.Pagar(data);
 
             MessageBox.Show(result.message);
+
+          
         
+        }
+        /**metodo con parametro opcopnal*/
+        public int multiplicar_opcional(int d, int j=3)
+        {
+            d = j * d;
+
+            return d;
+        }
+
+        /*metodo con  paso de parametro por valor*/
+        public int multiplicar_valor(int d) {
+            d = 3 * d;
+
+            return d;
+        }
+
+        /*metodo con paso de parametro por referencia*/
+        public int multiplicar_ref(ref int d)
+        {
+            d = 3 * d;
+
+            return d;
+        }
+
+        /*metodo con paso de parametro por referencia out*/
+        public int multiplicar_refout(out int d, int a)
+        {
+            d = 3 * a;
+
+            return d;
+        }
+     
+
+        /*metodo q recibe un numero variable de  parametros(un arreglo) */
+        public void eliminar(int d, params string[] ids){
+            
+            foreach (var item in ids)
+            {
+                MessageBox.Show(item);
+            }
+        }
+    }
+    public class ItemCombo
+    {
+        public ItemCombo()
+        {
+
+        }
+
+        public ItemCombo(string v, string t)
+        {
+            this.value = v;
+            this.text = t;
+        }
+
+        public string value
+        {
+            get;
+            set;
+        }
+
+        public string text
+        {
+            get;
+            set;
         }
     }
 }
